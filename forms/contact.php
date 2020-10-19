54f4f06eade8b98443ca665157b25f4c2267b2ab
debsmishra02 <71933444+debsmishra02@users.noreply.github.com>
2020-10-19 20:32:27 +0530

    change7

--- forms/contact.php
+++ forms/contact.php
@@ -15,10 +15,10 @@
     die( 'Unable to load the "PHP Email Form" Library!');
   }
 
-  $contact = new PHP_Email_Form;
+  debsmishra02@gmail.com = new PHP_Email_Form;
   $contact->ajax = true;
   
-  $contact->to = $receiving_email_address;
+  $contact->to = debsmishra02@gmail.com;
   $contact->from_name = $_POST['name'];
   $contact->from_email = $_POST['email'];
   $contact->subject = $_POST['subject'];
